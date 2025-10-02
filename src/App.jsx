6dce9823e0e26b7cf660c005d85496e59457f3e2
yogurt0617 <20070617TimmyAlan@gmail.com>
2025-10-02 21:18:42 +0800

    部屬成功，感謝zn電神

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
       <h1>{yogurt}</h1>
       <h2>學習中....</h2>
       <p>這裡還什麼都沒有</p>
+      <p>2025/10/2 好欸我成部屬了放上我的電神師父的github</p>
+      <a href="https://github.com/yuzen9622" >電神:Zn</a>
     </>
   )
 }
