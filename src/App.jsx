8d4d33b78f511ee0f86dd2accd93d36731472409
yogurt0617 <20070617TimmyAlan@gmail.com>
2025-10-03 18:21:27 +0800

    更新時間顯示方式並讓時間保持在頁面最上方、標題動畫

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { TypeAnimation } from 'react-type-animation'
 import './App.css'
 import "moment/dist/locale/zh-tw"
 import moment from 'moment' 
+import "animate.css"
 moment.locale("zh-tw")
 
 function App() {
   const [yogurt,setYogurt] = useState("こんにちは！yogurtの自己紹介です。")
   
-  const[nowTime,setNowTime] = useState(moment().format("YYYY MMMM Do a HH:mm:ss"))
+  const[nowTime,setNowTime] = useState(moment().format("YYYY MMM Do HH:mm:ss (ddd)"))
   const[nowHour,setNowHour] = useState(moment().format("HH"))
 
   useEffect(() =>{
     const timer = setInterval(()=>{
-      setNowTime(moment().format("YYYY MMMM Do a HH:mm:ss"))
+      setNowTime(moment().format("YYYY MMM Do HH:mm:ss (ddd)"))
     },1000)
     const timer2 = setInterval(()=>{
       setNowHour(moment().format("HH"))
@@ -32,17 +34,25 @@ function App() {
       setYogurt("こんばんは！yogurtの自己紹介です。")
     }
   },[nowHour])
-  
-  
 
+
+ 
   return (
     <>
     <header>
       <p id='nowTime'>{nowTime}</p>
     </header>
-
     <main>
-      <h1>{yogurt}</h1>
+      <div>
+      <TypeAnimation
+        key={yogurt}  
+        sequence={[yogurt, 1000]}  // 這裡把變數放進去
+        wrapper="span"
+        cursor={true}
+        repeat={Infinity}
+        style={{ fontSize: '3em', display: 'inline-block',fontWeight:"bolder"}}
+      />
+    </div>
       <h2>學習中....</h2>
       <p>這裡還什麼都沒有</p>
       <button onClick={()=>setNowHour("06")}>早上</button>
@@ -50,7 +60,37 @@ function App() {
       <button onClick={()=>setNowHour("00")}>晚上</button>
       <p>上面按鈕是測試用的下一次更新就會del掉了</p>
       <p>2025/10/2 好欸我成功部屬了放上我的電神師傅的github</p>
-      <a href="https://github.com/yuzen9622" >友站:Zn</a>
+      <a href="https://github.com/yuzen9622" target='_block'>友站:Zn</a>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>我是湊行數的</p>
+      <p>為了測試我的header正常，我湊了30行</p>
       </main>
     </>
   )
